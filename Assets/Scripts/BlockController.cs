918c87437f74596345a819fe57c6842d6b75d70c
estebanCaballero70200 <e.caballero.develop@gmail.com>
2022-05-03 20:38:34 -0300

    brocken blocks ready

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8660ed2bca21727c7f6ebf9163c2bc5e051885404c889896f432233fbb537e2b
-size 4062
+oid sha256:c880da4ea2f1765b45e9269fcd2bbe6550973603cc2245c6a88e789e5f755cdb
+size 4589
---
+++ Assets/Scripts/GameLevelScripts/BrakingSquare.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6a4922626fde1b24a9249027ffc77bd9b56e508105eba69f903074a39465cd5
+size 716
